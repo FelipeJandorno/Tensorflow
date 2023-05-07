928a5de35f8f21dcaa7783f21dae24bb5bff9bd4
Felipe Figueiredo <felipefjandorno@gmail.com>
2023-05-07 14:55:58 -0300

    Código completo
    Finalização da aula 02

--- Curso-inteligência-artificial-para-todos-yt/Aula 2.py
+++ Curso-inteligência-artificial-para-todos-yt/Aula 2.py
@@ -29,10 +29,36 @@ print(type(motores_treino))
 plt.scatter(motores_treino, co2_treino, color='blue')
 plt.xlabel("Motor")
 plt.ylabel("Emissão CO2")
-plt.show()
+#plt.show()
 
 # Exibir a correlação entre as features do dataset de teste
 plt.scatter(motores_teste, co2_teste, color='red')
 plt.xlabel("Motores")
 plt.ylabel("Emissão de CO2")
+#plt.show()
+
+# ================= CRIAÇÃO E TREINAMENTO DO MODELO =================
+modelo = linear_model.LinearRegression()
+modelo.fit(motores_treino, co2_treino)
+
+# Exibindo as saídas do modelo
+print('(A) Intercepto: ', modelo.intercept_) # valor de y quando x = 0
+print('(B) Inclinação: ', modelo.coef_) # coeficiente angular da reta
+
+# ================= PREDIÇÕES DO MODELO =================
+predicoesCo2 = modelo.predict(motores_teste)
+#print(predicoesCo2)
+
+plt.scatter(motores_teste, co2_teste, color='purple')
+plt.plot(motores_teste, modelo.coef_[0][0]*motores_teste + modelo.intercept_[0], '-r')
+plt.xlabel('Motores')
+plt.ylabel('Emissão de CO2')
 plt.show()
+
+# ================= MÉTRICAS DO MODELO =================
+print('=================== RELATÓRIO FINAL ===================')
+print('Soma dos erros ao quadrado (SSE): %.2f' % np.sum((predicoesCo2 - co2_teste)**2))
+print('Erro quadrático médio (MSE): %.2f' % mean_squared_error(co2_teste, predicoesCo2))
+print('Erro médio absoluto: %.2f' % mean_absolute_error(co2_teste, predicoesCo2))
+print('Raiz do erro quadrático médio (RMSE): %.2f' % sqrt(mean_squared_error(co2_teste, predicoesCo2)))
+print('R2-score: %.2f' % r2_score(predicoesCo2, co2_teste))
