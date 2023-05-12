727ad4e285670d98d1a7d22d0a26b654fc96c53d
FelipeJandorno <felipefjandorno@gmail.com>
2023-05-12 20:02:20 -0300

    Atualizações IA

--- Bases-Nitrogenadas/cnn-model.py
+++ Bases-Nitrogenadas/cnn-model.py
@@ -2,7 +2,7 @@ import os
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-#import sklearn as sk
+from sklearn.model_selection import train_test_split
 #import pyplot as plt
 
 # ================ PRÉ PROCESSAMENTO DE DADOS ================ #
@@ -45,19 +45,36 @@ df = pd.DataFrame(data=df_all, dtype="float64")
 for col in range(df.shape[1]):
     new_name = "Amostra {}".format(col)
     df = df.rename(columns={df.columns[col]: new_name})
-
+print(df)
 # ================ SEPARAÇÃO DE DADOS PARA TREINO E TESTE ================ #
-#(x_train, y_train), (x_test, y_test) =
+train, test = train_test_split(df, train_size=0.8)
 
 # ================ SEPARAÇÃO DAS FEATURES ================
-#y_class = ['Wavenumber', 'Adenine', 'Citosine', 'Ade + Cit']
+
 
 # ================ CRIAÇÃO DO MODELO ================
+print("======================TRAIN========================")
+print(train)
+print("===================TESTE===========================")
+print(test)
+print("===================DATAFRAME=======================")
+print(df)
 
-#model = tf.keras.Sequential()
-#model.add(tf.keras.layers.Conv2D(filters=35, activation='relu', kernel_size=3, padding='same'))
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv1D(filters=23, activation='relu', kernel_size=3, padding='same', input_shape=(df.shape)))
+model.add(tf.keras.layers.MaxPool1D(pool_size=2, strides=2, padding='valid'))
+#model.add(tf.keras.layers.Conv2D(filters=66, activation='relu', kernel_size=6, padding='same'))
+model.add(tf.keras.layers.Dropout(0.2))
+#model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2, padding='valid'))
+model.add(tf.keras.layers.Flatten(input_shape=(13062, 31)))
+model.add(tf.keras.layers.Dense(units=330, activation='relu'))
+model.add(tf.keras.layers.Dense(units=5, activation='softmax'))
 
 # ================ COMPILAÇÃO DO MODELO ================
-#model.compile()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+# ================ TREINAMENTO DO MODELO ================
+model.fit(train, epochs=3)
 
+# ================ AVALIAÇÃO DO MODELO ================
+#model.evaluate(test, verbose=2)
