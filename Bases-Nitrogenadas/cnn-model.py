f30498486e92ac8e0e5890691a78466b1982762c
Felipe Figueiredo <felipefjandorno@gmail.com>
2023-05-14 20:06:36 -0300

    Atualização
    Continuar organização dos dados na segunda :)

--- Bases-Nitrogenadas/cnn-model.py
+++ Bases-Nitrogenadas/cnn-model.py
@@ -1,44 +1,12 @@
 import tensorflow as tf
 import pandas as pd
+import os
 import numpy as np
-<<<<<<< Updated upstream
-=======
 from sklearn.model_selection import train_test_split
->>>>>>> Stashed changes
 
 
 # ================ PRÉ PROCESSAMENTO DE DADOS ================
 # Lê o arquivo CSV
-<<<<<<< Updated upstream
-df_c = pd.read_csv('./CSV/Amostra C - Thu Apr 27 17-28-37 2023 (GMT-03-00).CSV', delimiter=";")
-df_g = pd.read_csv('./CSV/Amostra G - Thu Apr 27 17-42-21 2023 (GMT-03-00).CSV', delimiter=";")
-df_h = pd.read_csv('./CSV/Amostra H - Thu Apr 27 18-39-46 2023 (GMT-03-00).CSV', delimiter=";")
-
-# Adicionando labels no dataframe
-df_c.columns = ['Wavenumber', 'Adenine']
-df_g.columns = ['Wavenumber', 'Citosine']
-df_h.columns = ['Wavenumber', 'Ade + Cit']
-
-# Trocando as vírgulas dos dados por ponto
-df_c['Wavenumber'], df_c['Adenine'] = df_c['Wavenumber'].str.replace(',', '.'), df_c['Adenine'].str.replace(',', '.')
-df_g['Wavenumber'], df_g['Citosine'] = df_g['Wavenumber'].str.replace(',', '.'), df_g['Citosine'].str.replace(',', '.')
-df_h['Wavenumber'], df_h['Ade + Cit'] = df_h['Wavenumber'].str.replace(',', '.'),\
-    df_h['Ade + Cit'].str.replace(',', '.')
-
-# Alterando o tipo de variável dos dataframes
-df_c['Wavenumber'], df_c['Adenine'] = df_c['Wavenumber'].astype('float64'), df_c['Adenine'].astype('float64')
-df_g['Wavenumber'], df_g['Citosine'] = df_g['Wavenumber'].astype('float64'), df_g['Citosine'].astype('float64')
-df_h['Wavenumber'], df_h['Ade + Cit'] = df_h['Wavenumber'].astype('float64'), df_h['Ade + Cit'].astype('float64')
-
-# Juntando dados em colunas diferentes
-df = df_c
-df['Citosine'] = df_g['Citosine']
-df['Ade + Cit'] = df_h['Ade + Cit']
-print(df.dtypes)
-
-# ================ SEPARAÇÃO DE DADOS PARA TREINO E TESTE ================
-
-=======
 def read_files(path):
     df_all = pd.DataFrame(dtype="float64")
     df_wv = pd.DataFrame(dtype="float64")
@@ -120,15 +88,10 @@ new_arr = []
 # ================ SEPARAÇÃO DE DADOS PARA TREINO E TESTE ================ #
 # train, test = train_test_split(df, train_size=0.8)
 # print('train: ', train.shape, ' test: ', test.shape)
->>>>>>> Stashed changes
 
 # ================ SEPARAÇÃO DAS FEATURES ================
-y_class = ['Wavenumber', 'Adenine', 'Citosine', 'Ade + Cit']
 
 # ================ CRIAÇÃO DO MODELO ================
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv2D(filters=35, activation='relu', kernel_size=3, padding='same'))
 
 # ================ COMPILAÇÃO DO MODELO ================
-model.compile()
