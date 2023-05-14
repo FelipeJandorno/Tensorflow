b44ef7f352f83f619bad5959e98f1b7dad2e22c2
Felipe Figueiredo <felipefjandorno@gmail.com>
2023-05-14 13:53:30 -0300

    Adição de coluna no df
    A coluna com a frequência das vibrações das moléculas foi adicionada ao DataFrame que será passado para o modelo

--- Bases-Nitrogenadas/cnn-model.py
+++ Bases-Nitrogenadas/cnn-model.py
@@ -2,13 +2,15 @@ import os
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-#import pyplot as plt
+# from sklearn.model_selection import train_test_split
+
+# import pyplot as plt
 
 # ================ PRÉ PROCESSAMENTO DE DADOS ================ #
 # Lê o arquivo CSV
 
 df_all = pd.DataFrame(dtype="float64")
+df_wv = pd.DataFrame(dtype="float64")
 path = "CSV/"
 
 for filename in enumerate(os.listdir(path)):
@@ -21,8 +23,14 @@ for filename in enumerate(os.listdir(path)):
     # Adicionando a label das colunas no dataframe
     df.columns = ['Wavenumber', 'Amostra {}'.format(filename[0])]
 
+    # Separação da frequência da luz
+    df_wv['Wavenumber {}'.format(filename[0])] = df['Wavenumber']
+
     # Juntando todos os dados em colunas no dataframe df_all
     df_all['Amostra {}'.format(filename[0])] = df['Amostra {}'.format(filename[0])]
+
+# Separando uma coluna de Wavenumber (todas são iguais)
+df_wv = df_wv['Wavenumber 0']
 
 # Procurando o maior valor do dataFrame
 df_all = df_all.to_numpy(dtype="float64")
@@ -45,36 +53,41 @@ df = pd.DataFrame(data=df_all, dtype="float64")
 for col in range(df.shape[1]):
     new_name = "Amostra {}".format(col)
     df = df.rename(columns={df.columns[col]: new_name})
-print(df)
+
+# Adicionando a coluna de Wavenumber no dataframe
+df['Wavenumber'] = df_wv
+# print(df)
 # ================ SEPARAÇÃO DE DADOS PARA TREINO E TESTE ================ #
-train, test = train_test_split(df, train_size=0.8)
+# train, test = train_test_split(df, train_size=0.8)
+# print('train: ', train.shape, ' test: ', test.shape)
 
 # ================ SEPARAÇÃO DAS FEATURES ================
 
 
 # ================ CRIAÇÃO DO MODELO ================
-print("======================TRAIN========================")
-print(train)
-print("===================TESTE===========================")
-print(test)
-print("===================DATAFRAME=======================")
-print(df)
+# print("======================TRAIN========================")
+# print(train)
+# print("===================TESTE===========================")
+# print(test)
+# print("===================DATAFRAME=======================")
+# print(df)
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv1D(filters=23, activation='relu', kernel_size=3, padding='same', input_shape=(df.shape)))
-model.add(tf.keras.layers.MaxPool1D(pool_size=2, strides=2, padding='valid'))
-#model.add(tf.keras.layers.Conv2D(filters=66, activation='relu', kernel_size=6, padding='same'))
-model.add(tf.keras.layers.Dropout(0.2))
-#model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2, padding='valid'))
-model.add(tf.keras.layers.Flatten(input_shape=(13062, 31)))
-model.add(tf.keras.layers.Dense(units=330, activation='relu'))
-model.add(tf.keras.layers.Dense(units=5, activation='softmax'))
+# model = tf.keras.Sequential()
+# model.add(tf.keras.layers.Conv1D(filters=23, activation='relu',
+                                # kernel_size=3, padding='same', input_shape=(df.shape)))
+# model.add(tf.keras.layers.MaxPool1D(pool_size=2, strides=2, padding='valid'))
+# #model.add(tf.keras.layers.Conv2D(filters=66, activation='relu', kernel_size=6, padding='same'))
+# model.add(tf.keras.layers.Dropout(0.2))
+# model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2, padding='valid'))
+# model.add(tf.keras.layers.Flatten(input_shape=(13062, 31)))
+# model.add(tf.keras.layers.Dense(units=330, activation='relu'))
+# model.add(tf.keras.layers.Dense(units=5, activation='softmax'))
 
 # ================ COMPILAÇÃO DO MODELO ================
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # ================ TREINAMENTO DO MODELO ================
-model.fit(train, epochs=3)
+# model.fit(train, epochs=3)
 
 # ================ AVALIAÇÃO DO MODELO ================
-#model.evaluate(test, verbose=2)
+# model.evaluate(test, verbose=2)
