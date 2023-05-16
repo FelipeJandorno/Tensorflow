85d0bb51cfc83400fcf0f140d1b55619b1d615a4
FelipeJandorno <felipefjandorno@gmail.com>
2023-05-16 15:00:09 -0300

    protótipo de leitura de dados funcional
    A função read_files5() possui um protótipo funcional da organização necessária para criar o dataframe. É preciso, contudo, excluir o primeiro item do dataframe para evitar duplicação de daddos

--- Bases-Nitrogenadas/read_files.py
+++ Bases-Nitrogenadas/read_files.py
@@ -29,4 +29,47 @@ def read_files2(path):
     df_array = np.array(list(new_df_dict.items()), dtype=object)
     print(df_array)
 
-read_files2("CSV/")
+def read_files3(path):
+    arr = np.array([[]])
+    for file in enumerate(os.listdir(path)):
+        df = pd.read_csv(path+file[1])
+        df = df.to_numpy()
+        print(arr.shape)
+        arr = np.insert(arr, df.shape[0], [df], axis=0)
+
+def read_files4():
+    df1 = pd.read_csv("CSV/Amostra C - Thu Apr 27 17-28-37 2023 (GMT-03-00).CSV")
+    df2 = pd.read_csv("CSV/Amostra H - Thu Apr 27 18-39-46 2023 (GMT-03-00).CSV")
+    df3 = pd.read_csv("CSV/Amostra G - Thu Apr 27 17-42-21 2023 (GMT-03-00).CSV")
+
+    df1 = df1.to_numpy()
+    df2 = df2.to_numpy()
+    df3 = df3.to_numpy()
+
+    arr = np.array([
+        df1,
+        df2
+    ])
+
+    arr = np.insert(arr, arr.shape[0], [df3], axis=0)
+
+    print(arr.shape)
+
+# read_files4()
+
+def read_file5(path):
+    df1 = pd.read_csv("CSV/Amostra C - Thu Apr 27 17-28-37 2023 (GMT-03-00).CSV")
+    df1 = df1.to_numpy()
+
+    arr = np.array([
+        df1
+    ])
+
+    for filename in os.listdir(path):
+        df = pd.read_csv(path+filename)
+        df = df.to_numpy()
+        arr = np.insert(arr, arr.shape[0], [df], axis=0)
+    print(arr.shape)
+
+
+read_file5("CSV/")
